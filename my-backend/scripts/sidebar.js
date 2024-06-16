96b277c56d6c20b783fd7a55268535aad599ecd8
d.augusto <d.augusto@accenture.com>
2024-06-16 06:53:31 -0300

    logout

--- my-backend/scripts/sidebar.js
+++ my-backend/scripts/sidebar.js
@@ -13,8 +13,26 @@ if (!sidebarCarregado) {
                 window.location.href = 'extrato.html'; // Redireciona para a página de extrato
             });
         }
+
+        // Adiciona o evento de logout ao botão #logoutBtn
+        const logoutBtn = document.getElementById('logoutBtn');
+        if (logoutBtn) {
+            logoutBtn.addEventListener('click', function (e) {
+                e.preventDefault();
+                logout();
+            });
+        }
     });
 
     // Atualiza a variável para indicar que o sidebar já foi carregado
     sidebarCarregado = true;
+}
+
+// Função de logout
+function logout() {
+    // Aqui você pode adicionar o código para realizar o logout, como limpar o token de autenticação
+    localStorage.removeItem('token'); // Exemplo de remoção de um token de autenticação do localStorage
+
+    // Redireciona para a página de login
+    window.location.href = 'login.html';
 }
